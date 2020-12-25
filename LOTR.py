0f6700f4ab18c70d6c95a0d61403d9e11c423c36
Lieven Geryl <55496214+LievenGeryl@users.noreply.github.com>
2020-12-25 10:43:46 +0100

    Added LOTR API functionality + LOTR playground

---
+++ LOTR.py
@@ -0,0 +1,44 @@
+import requests
+import random
+
+auth = "Bearer cUR_17Bs17xRvRzStIv4"
+#Books
+
+#response = requests.get("https://the-one-api.dev/v2/book", headers = {"Authorization": auth})
+#docs = response.json()["docs"]
+#books = [print(x['name']) for x in docs]
+
+
+#Movies
+#response = requests.get("https://the-one-api.dev/v2/movie", headers = {"Authorization": auth})
+#print(response.json())
+#docs = response.json()["docs"]
+#movie = [print(x['name']) for x in docs]
+
+#Characters
+
+#while True:
+   # randomId = random.randint(0, 940)
+    #response = requests.get("https://the-one-api.dev/v2/character", headers = {"Authorization": auth})
+ #   docs = response.json()["docs"]
+  #  print("Name: " + docs[randomId]["name"])
+   # print("Birth: " + docs[randomId]["birth"])
+#    print("Death: " + docs[randomId]["death"])
+ #   print("Race: " + docs[randomId]["race"])
+  #  print("Gender: " + docs[randomId]["gender"])
+   # print("From the realm of: " + docs[randomId]["realm"])
+   # print("Spouse: " + docs[randomId]["spouse"])
+    #print("Not everything is known about these characters, if the value is blank or NaN it means that it simply isn't known, isn't relevant or isn't the database yet.")
+    #print("====================================")
+    #breakVar = input("new character? (Y/N) ")
+    #if breakVar == "y" or breakVar == "Y":
+     #   continue
+    #elif breakVar == "n" or breakVar == "N":
+      #  break
+
+
+randomId = random.randint(0, 999)
+response = requests.get("https://the-one-api.dev/v2/quote", headers = {"Authorization": auth})
+docs = response.json()["docs"]
+
+print("Quote: '" + docs[randomId]["dialog"]+"'")
--- WebexBot.py
+++ WebexBot.py
@@ -1,9 +1,12 @@
 import requests
 import json
-import time
+import random
 
-#AccessToken, enkel 12u geldig
-accessToken = "Bearer YTFhNjVlODYtNzU5My00MzQ0LWEwNDEtODgxMTA4MjYyYTJmZDZmNDk2ZjctZjVh_PF84_consumer"
+#AccessToken Webex, enkel 12u geldig
+accessToken = "Bearer NDVkNTM0NjQtZmYwMy00MTU4LTllNTMtZjZiYWQ4ODQ1ZWU0OGU4OGY0ZTItZWM1_PF84_consumer"
+
+#Auth key voor LOTR API
+auth = "Bearer cUR_17Bs17xRvRzStIv4"
 
 #Rooms API gebruiken
 resp = requests.get(   "https://api.ciscospark.com/v1/rooms",
@@ -19,7 +22,7 @@ while True:
     # Input om aan te geven waar hij achter bot commands moet zoeken 
     zoekDit = input("Waar wil je dat de bot luistert? Geef de exacte naam in: ")
 
-    #roomId variabele
+    #roomId variabele setten
     roomId = None
     
     for room in rooms:
@@ -40,10 +43,7 @@ while True:
         break
 
 #Bot loop
-
 while True:
-    #1 seconde delay, ik wil niet over het limiet van API calls gaan
-    time.sleep(1)
 
     #parameters die ik mee geef bij het HTTP GET request
     getParameters = {
@@ -58,13 +58,68 @@ while True:
                     )
 
     jsonData = r.json()
-    # kijken als er berichten in de room zitten
+    # kijken als er berichten in de room zitten, zo niet, exception
     if len(jsonData["items"]) == 0:
         raise Exception("Geen berichten in de room")
     
-    # alle berichten opslaan
+    # alle berichten "opslaan"
     messages = jsonData["items"]
-    # tekst van het eerste bericht opslaan in een andere variabele
+    # tekst van het meest recente bericht opslaan in een andere variabele, met deze werken we.
     message = messages[0]["text"]
-    print("Ik zag dit bericht: " + message)
+
+
     
+    print("Ik zag dit bericht: " + message)
+
+    #help-menu voor bot
+    if message == "/help":
+        print("De volgende commando's kunt u uitvoeren: \n",
+              "help: Pretty obvious, isn't it?\n",
+              "movies: Geef alle Lord Of The Rings films via The One API\n",
+              "books: Geef alle Lord Of The Rings boeken via The One API\n",
+              "character: Geef een willekeurig karakter die in Tolkiens universum bestaat\n",
+              "quote: Geef een willekeurge quote van een karakter die in Tolkiens universum bestaat\n")
+
+    #Alle films printen
+    if message == "/movies":
+        response = requests.get("https://the-one-api.dev/v2/movie", headers = {"Authorization": auth})
+        print(response.json())
+        docs = response.json()["docs"]
+        movie = [print(x['name']) for x in docs]
+
+    #Alle boeken printen
+    if message == "/books":
+        response = requests.get("https://the-one-api.dev/v2/book", headers = {"Authorization": auth})
+        docs = response.json()["docs"]
+        books = [print(x['name']) for x in docs]
+
+    #print random character
+    if message == "/character":
+        while True:
+            randomId = random.randint(0, 940)
+            response = requests.get("https://the-one-api.dev/v2/character", headers = {"Authorization": auth})
+            docs = response.json()["docs"]
+            print("Name: " + docs[randomId]["name"])
+            print("Birth: " + docs[randomId]["birth"])
+            print("Death: " + docs[randomId]["death"])
+            print("Race: " + docs[randomId]["race"])
+            print("Gender: " + docs[randomId]["gender"])
+            print("From the realm of: " + docs[randomId]["realm"])
+            print("Spouse: " + docs[randomId]["spouse"])
+            print("Not everything is known about these characters, if the value is blank or NaN it means that it simply isn't known, isn't relevant or isn't the database yet.")
+            print("====================================")
+            breakVar = input("new character? (Y/N) ")
+            if breakVar == "y" or breakVar == "Y":
+                continue
+            elif breakVar == "n" or breakVar == "N":
+                break
+
+            
+    #print random quotes
+    if message == "/quote":
+        randomId = random.randint(0, 999)
+        response = requests.get("https://the-one-api.dev/v2/quote", headers = {"Authorization": auth})
+        docs = response.json()["docs"]
+        print("Quote: '" + docs[randomId]["dialog"]+"'")
+
+
